bb6ff661e9ff06f544313ac1b5269a97c141d132
luanym1234vip <camduyen20012@gmail.com>
2022-10-20 20:15:56 +0700

    update

---
+++ config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+  introTitle: "Hello mọi người 🥰",
+  introDesc: ``,
+  btnIntro: "Bấm tiếp vô đi nè",
+  title: "Chúc mọi người có 1 ngày 20/10 thật vui vẻ 😙",
+  desc: "Luôn hạnh phúc bên gia đình và bạn bè nhen 😘😘",
+  btnYes: "Yêu mọi người 😘😘",
+  btnNo: "Khum yêu ai hết  :v",
+  mess: "Tui biết mà 🥰. Yêu mọi người nhiều nhiều 😘😘",
+  messDesc: "Bấm nút kia mệt rùi đúng không :3",
+  btnAccept: "Ahihi đồ ngốk <3", //link mess của các bạn. VD: https://m.me/nam.nodemy
+};
